64edaafbbb7267e7f26accd06135c5ae9f078d38
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-02-05 18:17:42 +0100

    Small tweaks no real changes

--- src/dk/setogmap/lotto/Lotto.java
+++ src/dk/setogmap/lotto/Lotto.java
@@ -1,2 +1,53 @@
-package dk.setogmap.lotto;public class Lotto {
+package dk.setogmap.lotto;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+
+public class Lotto {
+    static HashSet<Integer> lotteryTicket = new HashSet<>();
+    static HashMap<Integer,Integer> lotteryNumbers = new HashMap<>();
+    static Random random = new Random();
+    public static void main(String[] args){
+        generateLottoryNumbers();
+        generateLotteryTicket();
+        checkLotteryTicket();
+    }
+    public static void generateLotteryTicket(){
+        boolean success = false;
+        while(!success){
+            int ticketNumber = random.nextInt(70)+1;
+            lotteryTicket.add(ticketNumber);
+            if(lotteryTicket.size() == 7){
+                success = true;
+            }
+        }
+    }
+    public static void generateLottoryNumbers(){
+        for(int mapKey = 1; mapKey < 10; mapKey++){
+            int lotteryNumber = random.nextInt(70)+1;
+            if(!lotteryNumbers.containsValue(lotteryNumber)) {
+                lotteryNumbers.put(mapKey, lotteryNumber);
+            }
+            else{
+                mapKey--;
+            }
+        }
+    }
+    public static void checkLotteryTicket(){
+        int winningNumberCount = 0;
+        int bonusNumberCount = 0;
+        for(int winningNumbers = 1; winningNumbers <= lotteryNumbers.size()-2; winningNumbers++){
+            if(lotteryTicket.contains(lotteryNumbers.get(winningNumbers))){
+                winningNumberCount++;
+            }
+        }
+        for(int bonusNumbers = lotteryNumbers.size()-2; bonusNumbers <= lotteryNumbers.size(); bonusNumbers++){
+            if(lotteryTicket.contains(lotteryNumbers.get(bonusNumbers))){
+                bonusNumberCount++;
+            }
+        }
+        System.out.println("Amount of winning numbers: " + winningNumberCount);
+        System.out.println("Amount of bonus numbers: " + bonusNumberCount);
+    }
 }
--- src/dk/setogmap/wordcount/WordCount.java
+++ src/dk/setogmap/wordcount/WordCount.java
@@ -1,2 +1,52 @@
-package dk.setogmap.wordcount;public class WordCount {
+package dk.setogmap.wordcount;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class WordCount {
+    public static void main(String[] args){
+        ArrayList<String> wordList = new ArrayList<>(){{
+           add("Ukraine");
+           add("Covid");
+           add("DF");
+           add("Putin");
+           add("Biden");
+        }};
+        try {
+            System.out.println(wordCountFromWebpage(wordList));
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+    public static HashMap<String,Integer> wordCountFromWebpage(ArrayList<String> list) throws IOException {
+        HashMap<String,Integer> updatedMap = new HashMap<>();
+        URL connection = new URL("https://dr.dk/");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.openStream()));
+        String inputLine = "";
+        while(inputLine != null){
+            wordCounter(inputLine, list, updatedMap);
+            inputLine = reader.readLine();
+        }
+        reader.close();
+        return updatedMap;
+    }
+
+    private static HashMap<String, Integer> wordCounter(String input, ArrayList<String> words, HashMap<String, Integer> tempMap) {
+        String[] tempStr = words.toArray(new String[0]);
+        for(String testStr: tempStr){
+            if(input.contains(testStr)){
+                if(tempMap.containsKey(testStr)){
+                    tempMap.put(testStr, tempMap.get(testStr)+1);
+                }
+                else{
+                    tempMap.put(testStr,1);
+                }
+            }
+        }
+        return tempMap;
+    }
 }
